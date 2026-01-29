d31e05e6ae8c68c1e6a9e614800209fb79976a78
Jesus <52017003+jsvJesus@users.noreply.github.com>
2026-01-29 23:36:20 +0200

    Re-new

--- Source/UEStalker/Public/Items/MasterItemDataAsset.h
+++ Source/UEStalker/Public/Items/MasterItemDataAsset.h
@@ -7,55 +7,102 @@
 #include "MasterItemDataAsset.generated.h"
 
 /**
- * Тут дизайнер заполняет данные предмета (конфиги).
- * Runtime значения (CurrDurability/CurrCharge/Stack) живут в ItemObject.
+ * БАЗОВЫЙ класс.
+ * Содержит только то, что есть у ЛЮБОГО предмета (имя, вес, иконка, цена).
  */
-UCLASS(BlueprintType)
+UCLASS(BlueprintType, Abstract) // Abstract не даст создать этот ассет напрямую, только наследников
 class UMasterItemDataAsset : public UPrimaryDataAsset
 {
 	GENERATED_BODY()
 
 public:
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Item|Details")
+	// Общие детали (Имя, Вес, Категория, Размер)
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Item|Common")
 	FMasterItemDetails ItemDetails;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Item|Trade")
+	// Торговля нужна всем
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Item|Common")
 	FItemTradeConfig TradeConfig;
 
-	// Показываем только если предмет может иметь прочность (Оружие, Броня, Шлем и т.д.)
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Item|Durability", 
-		meta=(EditCondition="ItemDetails.ItemCategory == EItemCategory::ItemCat_Weapons || ItemDetails.ItemCategory == EItemCategory::ItemCat_Armor || ItemDetails.ItemCategory == EItemCategory::ItemCat_Helmet", EditConditionHides))
-	FItemDurabilityConfig DurabilityConfig;
-
-	// Показываем, например, только для UsableItems (ПДА, Детекторы)
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Item|Charge",
-		meta=(EditCondition="ItemDetails.ItemCategory == EItemCategory::ItemCat_UsableItems", EditConditionHides))
-	FItemChargeConfig ChargeConfig;
-
-	// Показываем для Брони, Шлемов, Рюкзаков
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Item|Stats|Outfit",
-		meta=(EditCondition="ItemDetails.ItemCategory == EItemCategory::ItemCat_Armor || ItemDetails.ItemCategory == EItemCategory::ItemCat_Helmet || ItemDetails.ItemCategory == EItemCategory::ItemCat_Backpack", EditConditionHides))
-	FItemOutfitStatsConfig OutfitStatsConfig;
-
-	// Показываем ТОЛЬКО если категория Weapons
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Item|Stats|Weapon",
-		meta=(EditCondition="ItemDetails.ItemCategory == EItemCategory::ItemCat_Weapons", EditConditionHides))
-	FItemWeaponsStatsConfig WeaponStatsConfig;
-
-	// Показываем только для Магазинов
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Item|Stats|Magazine",
-		meta=(EditCondition="ItemDetails.ItemSubCategory == EItemSubCategory::ItemSubCat_Attachments_Magazine", EditConditionHides))
-	FItemMagazineConfig MagazineConfig;
-
-	// Показываем для Еды, Воды, Медицины
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Item|Stats|Consumable",
-		meta=(EditCondition="ItemDetails.ItemSubCategory == EItemSubCategory::ItemSubCat_Items_Water || ItemDetails.ItemSubCategory == EItemSubCategory::ItemSubCat_Items_Food || ItemDetails.ItemSubCategory == EItemSubCategory::ItemSubCat_Items_Medicine", EditConditionHides))
-	FConsumablesStats ConsumablesStats;
-
-	// Чтобы в PrimaryAsset системе было красиво
 	virtual FPrimaryAssetId GetPrimaryAssetId() const override
 	{
-		// Type = "MasterItem", Name = AssetName
 		return FPrimaryAssetId(TEXT("MasterItem"), GetFName());
 	}
+};
+
+/**
+ * ОРУЖИЕ (Weapon)
+ * Добавляет статы оружия, магазины, прочность.
+ */
+UCLASS(BlueprintType)
+class UWeaponDataAsset : public UMasterItemDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Item|Weapon Stats")
+	FItemWeaponsStatsConfig WeaponStatsConfig;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Item|Weapon Stats")
+	FItemDurabilityConfig DurabilityConfig;
+
+	// Если нужно, сюда можно добавить совместимые прицелы и т.д.
+};
+
+/**
+ * ЭКИПИРОВКА (Armor, Helmet, Backpack)
+ * Добавляет защиту, прочность, слоты контейнеров.
+ */
+UCLASS(BlueprintType)
+class UOutfitDataAsset : public UMasterItemDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Item|Outfit Stats")
+	FItemOutfitStatsConfig OutfitStatsConfig;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Item|Outfit Stats")
+	FItemDurabilityConfig DurabilityConfig;
+};
+
+/**
+ * РАСХОДНИКИ (Food, Meds)
+ * Добавляет эффекты лечения/еды.
+ */
+UCLASS(BlueprintType)
+class UConsumableDataAsset : public UMasterItemDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Item|Consumable Stats")
+	FConsumablesStats ConsumablesStats;
+};
+
+/**
+ * ГАДЖЕТЫ (PDA, Detectors)
+ * Добавляет заряд батареи.
+ */
+UCLASS(BlueprintType)
+class UUsableItemDataAsset : public UMasterItemDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Item|Device Stats")
+	FItemChargeConfig ChargeConfig;
+};
+
+/**
+ * МАГАЗИНЫ / АТТАЧМЕНТЫ
+ */
+UCLASS(BlueprintType)
+class UAttachmentDataAsset : public UMasterItemDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Item|Attachment")
+	FItemMagazineConfig MagazineConfig;
 };
